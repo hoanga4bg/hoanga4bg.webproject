a0980d035ba2ae85b0d63f40d3da0d9911fc739c
hoanga4bg <hoanga4bg@gmail.com>
2020-12-13 14:50:12 +0700

    v

--- src/main/java/com/laptrinhweb/ProjectWebApplication.java
+++ src/main/java/com/laptrinhweb/ProjectWebApplication.java
@@ -1,5 +1,7 @@
 package com.laptrinhweb;
 
+import javax.annotation.sql.DataSourceDefinition;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
